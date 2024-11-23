f9cd8c8b5226dd30161e0af4e86019289ad850a7
Abhishek Chandrasekar <aabhishekcs96@gmail.com>
2024-11-23 15:50:39 +1100

    re-visit

--- dev_logger.py
+++ dev_logger.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 import os
 from datetime import datetime
+import yaml
 
 # Content keywords below:
 # Tasks, Progress, Challenges, Solutions, Decisions, Learnings, Next Steps, Additional Notes, Conclusion
 
 backslash_character = chr(10)
+
+
+def find_config_file(project_dir, config_file_name):
+    try:
+        project_config_file = os.path.join(project_dir, config_file_name)
+        print(project_config_file)
+        if not os.path.exists(project_config_file):
+            default_config_file = os.path.expanduser(os.path.join('~', '.config', 'project_config.yaml'))
+            project_config_file = default_config_file
+        return project_config_file
+    except FileNotFoundError as e:
+        print(f'Error: {e} - File not found')
+
+
+def read_config_file(config_file):
+    with open(config_file, "r") as file:
+        config = yaml.safe_load(file)
+        headings = list(config['headings'])
+        if isinstance(headings, list):
+            return headings
+        else:
+            raise ValueError("Config file does not contain top level list")
 
 def add_content(content_type):
     contents = []
@@ -35,18 +58,14 @@ def write_to_log_file(file_path, markdown_content):
 
 
 if __name__ == "__main__":
-    today = datetime.today().strftime("%Y-%m-%d")
+    today = datetime.today().strftime("%Y-%m-%d-%s")
     file_path = f"dev-logs/{today}_dev_log.md"
-    headings_to_complete = (
-        "tasks",
-        "challenges",
-        "solutions",
-        "decisions_made",
-        "learnings",
-        "next_steps",
-        "additional_notes",
-        "conclusion",
-    )
+    
+    project_dir = './config'
+    config_file_name = 'project_config.yaml'
+
+    config_file = find_config_file(project_dir, config_file_name)
+    headings = read_config_file(config_file=config_file)
 
     project_name = input("Enter the project name: ")
     author_name = input("Enter your name: ")
@@ -65,7 +84,7 @@ if __name__ == "__main__":
 """
     write_to_log_file(file_path, markdown_content)
 
-    for heading_to_complete in headings_to_complete:
+    for heading_to_complete in headings:
         points = add_content(heading_to_complete)
         markdown_content = generate_dev_log(heading_to_complete, points)
         write_to_log_file(file_path, markdown_content)
